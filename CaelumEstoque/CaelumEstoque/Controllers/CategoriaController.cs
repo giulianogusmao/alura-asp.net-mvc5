32bb770f532fae233c4d4dc5d51c0c09915490a7
Giuliano Gusmão <gusmaoggg@gmail.com>
2017-11-23 13:07:29 -0200

    Cadastrando produtos

---
+++ CaelumEstoque/CaelumEstoque/Controllers/CategoriaController.cs
@@ -0,0 +1,35 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        // GET: Categorias
+        public ActionResult Index()
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = dao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(CategoriaDoProduto categorai)
+        {
+            CategoriasDAO dao = new CategoriasDAO();
+            dao.Adiciona(categorai);
+            return RedirectToAction("Index", "Categoria");
+        }
+    }
+}
---
+++ CaelumEstoque/CaelumEstoque/Controllers/ProdutoController.cs
@@ -0,0 +1,39 @@
+﻿using CaelumEstoque.DAO;
+using CaelumEstoque.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CaelumEstoque.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            IList<Produto> produtos = dao.Lista();
+            ViewBag.Produtos = produtos;
+            return View();
+        }
+
+        public ActionResult Form()
+        {
+            CategoriasDAO categoriasDao = new DAO.CategoriasDAO();
+            IList<CategoriaDoProduto> categorias = categoriasDao.Lista();
+            ViewBag.Categorias = categorias;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Adiciona(Produto produto)
+        {
+            ProdutosDAO dao = new ProdutosDAO();
+            dao.Adiciona(produto);
+            return RedirectToAction("Index", "Produto");
+        }
+    }
+}
